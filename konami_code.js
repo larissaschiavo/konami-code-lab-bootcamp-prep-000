710598c0d4a22587c2d53bbcac4349d23f3c0d38
Larissa S <larissafschiavo@gmail.com>
2017-09-02 02:25:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,10 +3,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let idx = 0;
 
 //add event listener to document.body and check for 'keydown'
-
+const main = document.querySelector('document.body')
 function init() {
 
-const main = document.querySelector('document.body')
+
 
 main.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.which);
