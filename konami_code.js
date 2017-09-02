2366270b0c4cab9788938c5757f62461eb1f19c4
Larissa S <larissafschiavo@gmail.com>
2017-09-02 02:17:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let idx = 0; 
+let idx = 0;
+
+//add event listener to document.body and check for 'keydown'
+
+const main = document.querySelector('document.body')
+
+//if user presses in order alert() a congrats message
+
+//if user puts in one key out of order, keep listening until all keys in order have been pressed
 
 function init() {
-
 
 }
