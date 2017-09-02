218db1998c7fa133c5a6b5de30df1e3eaa4ccfde
Larissa S <larissafschiavo@gmail.com>
2017-09-02 02:28:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,17 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 function init() {
-let idx = 0;
+  let idx = 0;
 
-document.body.addEventListener('keydown', function(e){
-  const key = parseInt(e.detail || e.which);
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
     if (key === code[idx]) {
       idx++;
-    if (idx === code.length) {
-      alert("congrats!")
-      idx = 0;}
-    } else {
-    idx = 0;
+      if (idx === code.length) {
+        alert("congrats!")
+        idx = 0;}
+      } else {
+        idx = 0;
+      }
+    })
   }
-})
-}
