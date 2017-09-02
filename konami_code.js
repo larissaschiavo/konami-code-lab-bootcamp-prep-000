fdd80516489dfa4ec168ba3868dfa5482c47873c
Larissa S <larissafschiavo@gmail.com>
2017-09-02 02:26:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ const main = document.querySelector('document.body')
 
 
 
-main.addEventListener('keydown', function(e){
+document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.which);
 
   if (key === code[idx]) {
