aea31448b02765d16abb41ebdb075e6273728353
Larissa S <larissafschiavo@gmail.com>
2017-09-02 02:28:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,14 +8,12 @@ let idx = 0;
 
 document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.which);
-
-  if (key === code[idx]) {
-    idx++;
+    if (key === code[idx]) {
+      idx++;
     if (idx === code.length) {
       alert("congrats!")
-      idx = 0;
-    }
-  } else {
+      idx = 0;}
+    } else {
     idx = 0;
   }
 })
